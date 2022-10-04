0ae01dc493e508b1b58b618391753172e02c9d47
cheeoreeo <keeperthesky@gmail.com>
2022-10-04 10:34:22 +0500

    add new data

--- app/main.py
+++ app/main.py
@@ -24,9 +24,9 @@ class Posts(BaseModel):
 @app.get('/posts')
 async def get_posts(params: Posts = Depends()):
     page_filters = {
-        1: 'avito.txt',
-        2: 'avito2.txt',
-        3: 'avito3.txt'
+        1: 'yandex.txt',
+        2: 'yandex2.txt',
+        3: 'yandex3.txt'
     }
     result = read_from_file(file_name=page_filters[params.page])
     return {'posts': result}
--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,7 @@
 import json
 from string import digits
 import random
+import os
 
 
 def generate_vendor_code() -> str:
@@ -8,6 +9,8 @@ def generate_vendor_code() -> str:
 
 
 def write_to_file(posts: list, file_name: str) -> None:
+    if not os.path.isfile(file_name):
+        open(file_name, 'a').close()
     with open(file_name, 'w') as f:
         for post in posts:
             f.write(str(post) + '\n')
